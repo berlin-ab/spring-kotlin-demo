793e4027dbf60a3b89ed790bbbce38a795b1fa4f
Adam Berlin <aberlin@Adams-MacBook-Pro-2.local>
2017-06-14 16:42:29 -0400

    Move the JPA configuration to the more appropriate place.

--- components/blog-storage/src/blogStorage/BlogStorageApp.kt
+++ components/blog-storage/src/blogStorage/BlogStorageApp.kt
@@ -1,3 +1,13 @@
 package blogStorage
 
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
 interface BlogStorageApp
+
+
+@EnableJpaRepositories
+@EntityScan
+@Configuration
+open class BlogStorageConfig
--- containers/spring-boot-web-app/src/demo/DemoApplication.kt
+++ containers/spring-boot-web-app/src/demo/DemoApplication.kt
@@ -5,8 +5,6 @@ import blog.BlogApp
 import blogStorage.BlogStorageApp
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication(
         scanBasePackageClasses = arrayOf(
@@ -15,12 +13,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
                 BlogApp::class,
                 BlogStorageApp::class
         )
-)
-@EnableJpaRepositories(
-        basePackageClasses = arrayOf(BlogStorageApp::class)
-)
-@EntityScan(
-        basePackageClasses = arrayOf(BlogStorageApp::class)
 )
 class DemoApplication
 
